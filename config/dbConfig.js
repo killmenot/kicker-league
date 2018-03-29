72cf36e2e5df41588ffe1399a417cd8607455716
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-03-29 18:21:02 +0300

    prepare for deploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,5 +1,13 @@
 'use strict';
 
+if (process.env.CLEARDB_DATABASE_URL) {
+  const match = process.env.CLEARDB_DATABASE_URL.match(/mysql:\/\/([^:]+):([^@]+)@([^:]+)\/(.+)\?/);
+  process.env.DB_USERNAME = match[1];
+  process.env.DB_PASSWORD = match[2];
+  process.env.DB_HOSTNAME = match[3];
+  process.env.DB_NAME = match[4];
+}
+
 module.exports = {
   development: {
     dialect: 'sqlite',
@@ -21,6 +29,11 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
     dialect: 'mysql',
+    protocol: 'mysql',
+    port: 3306,
+    dialectOptions: {
+      ssl: true
+    },
     define: {
       underscored: true
     }
